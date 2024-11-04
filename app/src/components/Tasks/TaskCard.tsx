916e8f91aa2ededf7177cf571fc42c0a90a826b1
Jordan Sinou <sinoujordan@gmail.com>
2024-11-05 00:15:21 +0100

    date post update

--- app/src/components/Tasks/TaskCard.tsx
+++ app/src/components/Tasks/TaskCard.tsx
@@ -1,8 +1,12 @@
 import { Card, CardContent, Typography } from '@mui/joy';
 import { Task } from '@daily/shared/models';
+import { DateService } from '@daily/shared/services';
+import { useMemo } from 'react';
 
 export const TaskCard = ({ task }: Task) => {
   const { id, title, description, author, updatedAt } = task;
+  const dateService = useMemo(() => new DateService(), []);
+  let relativeDate = dateService.getRelativeDate(updatedAt);
 
   return (
     <Card key={id}>
@@ -10,7 +14,7 @@ export const TaskCard = ({ task }: Task) => {
         <Typography level={'h3'}>{title}</Typography>
         <Typography level="body-lg">{description}</Typography>
         <p>{author.name}</p>
-        <p>{`${updatedAt}`}</p>
+        <p>{relativeDate}</p>
       </CardContent>
     </Card>
   );
--- app/src/shared/services/src/DateService.ts
+++ app/src/shared/services/src/DateService.ts
@@ -1,11 +1,20 @@
 import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 export class DateService {
-  private sanitizeDate(date: dayjs.Dayjs): string {
+  constructor() {
+    dayjs.extend(relativeTime);
+  }
+
+  private sanitizeDate(date: string): string {
     return dayjs(date).format('DD-MM-YYYY');
   }
 
+  getRelativeDate(date: string): string {
+    return dayjs(+date).fromNow().toString();
+  }
+
   public getTodaysDate(): string {
-    return this.sanitizeDate(dayjs());
+    return this.sanitizeDate(dayjs().toString());
   }
 }
